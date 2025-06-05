6e7651e2df4c097e806474f27f51fe6de39d4fea
Luis Diasgiron <189465404+lolitec@users.noreply.github.com>
2025-06-05 18:31:48 +0000

    UPDATED calculadora para que no acepte valores vacios y que no se borren los numeros al realizar la operacion

--- aplicaciones_web/calculadora/app.py
+++ aplicaciones_web/calculadora/app.py
@@ -12,21 +12,30 @@ class Index:
     
     def POST(self):
         formulario = web.input()
-        n1 = int(formulario.inp_numero1)
-        n2 = int(formulario.inp_numero2)
-        if "btn_su" in formulario:
-            return render.calculadora(n1+n2)
-        elif "btn_re" in formulario:
-            return render.calculadora(n1-n2)
-        elif "btn_mu" in formulario:
-            return render.calculadora(n1*n2)
-        elif "btn_di" in formulario:
-            try:
-                return render.calculadora(round(float(n1)/float(n2),2))
-            except ZeroDivisionError:
-                return render.calculadora(0)
+        print(formulario)
+        n1 = formulario.inp_numero1
+        n2 = formulario.inp_numero2
+        if n1 == "":
+            return render.calculadora("Falta el primer numero",0,0)
+        elif n2 == "":
+            return render.calculadora("Falta el segundo numero",0,0)
         else:
-            return render.calculadora(0)
+            operacion = formulario.btn_operacion
+            n1 = int(n1)
+            n2 = int(n2)
+            if operacion == "suma":
+                return render.calculadora(n1+n2,n1,n2)
+            elif operacion == "resta":
+                return render.calculadora(n1-n2,n1,n2)
+            elif operacion == "multiplicacion":
+                return render.calculadora(n1*n2,n1,n2)
+            elif operacion == "division":
+                try:
+                    return render.calculadora(round(float(n1)/float(n2),2),n1,n2)
+                except ZeroDivisionError:
+                    return render.calculadora("Error, divison por 0")
+            else:
+                return render.calculadora(0)
 
 if __name__ == "__main__":
     app.run()
